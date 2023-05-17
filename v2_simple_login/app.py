206cd1500d150ef873c25b510bad8c9a28d1c3a1
ALPER AYDIN <info@alperaydin.net>
2023-05-17 21:31:10 +0300

    Update app.py

--- v2_simple_login/app.py
+++ v2_simple_login/app.py
@@ -1,27 +1,7 @@
-from flask import Flask, json, render_template, render_template_string, redirect, url_for, jsonify, request, session
-from flask_session import Session
-import random
-import pandas as pd
-import time
-import zipfile
-import datetime as dt
-import numpy as np
-import pickle as pkl
-
-app = Flask(__name__,
-            static_folder='static',
-            template_folder='templates'
-            )
-
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_TYPE'] = "filesystem"
-Session(app)
-
-data_path = 'static/data/'
-
-
 import os
 from flask import Flask, render_template, render_template_string, jsonify
+import teknokent_scraper as tns
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
@@ -37,7 +17,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-app.config["SECRET_KEY"] = "abc123"
+app.config["SECRET_KEY"] = "ENTER YOUR SECRET KEY"
 db = SQLAlchemy()
  
 login_manager = LoginManager()
@@ -116,3 +96,24 @@ def home():
 
 # /login ---------------------------------------------------------------------------------
 
+# universal function to call pages without explicit routing but having template files
+@app.errorhandler(404)
+def not_found(e):
+    url = request.path
+    try:
+        return render_template(f'{url}.html')
+    except:
+        return render_template('error404.html', url=url)
+
+# ---------------------------------------------------------------------------------
+
+
+
+
+@app.route('/')
+def init():
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
